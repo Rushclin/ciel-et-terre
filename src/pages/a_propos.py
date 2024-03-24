4cff667028c5a7a68343a193339c1807413232ae
Rushclin Takam <takamrushclin@gmail.com>
2024-03-24 03:38:44 +0100

    Page a propos

--- src/pages/a_propos.py
+++ src/pages/a_propos.py
@@ -1,5 +1,34 @@
 from dash import html
+import dash_mantine_components as dmc
+from utils import title
+
+
+def head(text):
+    return dmc.Text(text, align="center", my=10, mx=0)
 
 
 def page_a_propos(args):
-    return html.Div("La page a propos")
+    return html.Div(
+        style={
+            'display': 'flex',
+            'justify-content': 'center',
+            'align-items': 'center',
+            'height': '70vh',  
+            'text-align': 'center'
+        },
+        children=[
+            dmc.Container(
+                size="lg",
+                mt=5,
+                children=[
+                    title(
+                        args.APP_NAME,
+                    ),
+                    head(
+                        "Nous conçevons des dashboards specifiques à votre demande. Des Dashboards pour la production. "
+                    ),
+                    head("Code - Build - Test - Release. "),
+                ],
+            )
+        ]
+    )
